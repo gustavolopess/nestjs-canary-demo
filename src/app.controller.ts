e4fc2b147fe366428b991e2d9d8a404d13bf0229
Gustavo H. Lopes de Souza <gustavo.hlopess@gmail.com>
2021-06-27 10:37:20 -0300

    fix: adjust probability behavior

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,16 +5,16 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/error/:prob')
+  @Get('error/:prob')
   randomError(@Param('prob') errorProbability: number): string {
-    if (Math.random() < errorProbability) {
+    if (Math.random() > errorProbability / 100) {
       return this.appService.getVersion();
     }
 
     throw new InternalServerErrorException('Error');
   }
 
-  @Get('/sleep/:milliseconds')
+  @Get('sleep/:milliseconds')
   async sleepAndReturn(@Param('milliseconds') milliseconds: number): Promise<string> {
     await new Promise(r => setTimeout(r, milliseconds));
 
